a051d6f0da51248febafd817a08e5511e7a4bf58
SilenYang <yangyh19930308@gmail.com>
2019-12-11 22:54:28 +0800

    feat: update

--- src/pages/components/lunches/index.tsx
+++ src/pages/components/lunches/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
-import { Spin } from "antd";
+import { Spin, Icon } from "antd";
 import style from "./index.module.scss";
+import history from "utils/history";
 
 const todo = gql`
-  query {
-    todolist @cache
+  {
+    todolist @client {
+      id
+      title
+    }
   }
 `;
 
@@ -15,6 +19,16 @@ export default () => {
 
   return (
     <div className={style.container}>
+      <span className={style.title}>
+        <span
+          className={style.icon}
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          <Icon type="arrow-left" style={{ marginRight: "10px" }} />
+        </span>
+      </span>
       {loading && <Spin />}
       {error && <p>Error :(</p>}
       {data && (
